0525a167c14540ca3b8fd99b2d7a8042a2c29ff9
killianmh <killianmh@gmail.com>
2018-06-14 11:34:16 -0700

    package.json ready for deploy and hompage link corrected

--- clickygame/src/components/Header/Header.js
+++ clickygame/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = props => {
     return(
     <nav className="navbar nav-justified">
-        <a className="navbar-brand nav-item pg-title" href="#">Click-It!</a>
+        <a className="navbar-brand nav-item pg-title" href="https://killianmh.github.io/clickyGame/">Click-It!</a>
         <span className="navbar-brand nav-item msg"> { props.message }</span>
         <span className="navbar-brand nav-item score"> {`Score: ${props.score} | Top Score: ${props.topscore}`}</span>
     </nav>
